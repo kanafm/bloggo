e812fa7dbac73798921c881f2433cae2d1f0520e
Kana 🐠 <rekanacoding@gmail.com>
2026-07-02 15:50:29 -0500

    feat: add basic processing pipeline

--- bloggo.go
+++ bloggo.go
@@ -1,39 +1,134 @@
 package bloggo
 
 import (
+	"errors"
 	"fmt"
-	"io"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-type Bloggo struct {
-	out io.Writer
+type BuildRequest struct {
+	EntryPoints []string
+	OutDir      string
+	Processors  []Processor
 }
 
-func Of() *Bloggo {
-	return &Bloggo{out: os.Stdout}
+type Bloggable interface {
+	Build(request BuildRequest) error
 }
 
-func OfWriter(w io.Writer) *Bloggo {
-	return &Bloggo{out: w}
+type Bloggo struct{}
+
+func New() *Bloggo {
+	return &Bloggo{}
 }
 
-func (w *Bloggo) Writeln(text string) *Bloggo {
-	fmt.Fprintln(w.out, text)
-	return w
+func (b Bloggo) handle(path string, outdir string, processors []Processor) error {
+	// select the processor
+	var activeProcessor *Processor = nil
+	for _, processor := range processors {
+		if !processor.CanHandle(path) {
+			continue
+		}
+		activeProcessor = &processor
+		break
+	}
+
+	if activeProcessor == nil {
+		return errors.New("No processors can handle " + path)
+	}
+
+	// create directory for output file
+	ext := filepath.Ext(path)
+	name := strings.TrimSuffix(filepath.Base(path), ext)
+
+	absoluteOutDir, err := filepath.Abs(outdir)
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(absoluteOutDir, 0o755)
+	if err != nil {
+		return err
+	}
+
+	outFile := fmt.Sprintf("%s/%s.html", absoluteOutDir, name)
+
+	// handle it with processor
+	err = (*activeProcessor).Handle(path, outFile)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("\t. -> ", outFile)
+	return nil
 }
 
-func (w *Bloggo) Write(text string) *Bloggo {
-	fmt.Fprint(w.out, text)
-	return w
+func (b Bloggo) Build(request BuildRequest) error {
+	for ei := range request.EntryPoints {
+		e := request.EntryPoints[ei]
+
+		err := filepath.WalkDir(e, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			if d.IsDir() {
+				return nil
+			}
+
+			//path is absolute path
+			fmt.Println(". processing ", path)
+			err = b.handle(path, request.OutDir, request.Processors)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return nil
 }
 
-func (w *Bloggo) Writef(format string, args ...any) *Bloggo {
-	fmt.Fprintf(w.out, format, args...)
-	return w
+type Processor interface {
+	// CanHandle determines whether this processor can handle this file
+	CanHandle(fileLocation string) bool
+
+	// Handle processes file at file location, writing to output file
+	Handle(fileLocation string, outputFile string) error
 }
 
-func (w *Bloggo) SayHello() *Bloggo {
-	fmt.Fprintln(os.Stdout, "Hello, World!")
-	return w
+type NoOpProcessor struct{}
+
+func (NoOpProcessor) CanHandle(fileLocation string) bool {
+	return true
+}
+
+func (n NoOpProcessor) Handle(fileLocation string, outputFile string) error {
+	if !n.CanHandle(fileLocation) {
+		return errors.New("Cannot handle file: " + fileLocation)
+	}
+
+	data, err := os.ReadFile(fileLocation)
+	if err != nil {
+		return err
+	}
+
+	htmlified := fmt.Sprintf(
+		"<div><pre style=\"font-family: ui-monospace, monospace; margin:0;\">%s</pre></div>",
+		string(data))
+
+	err = os.WriteFile(outputFile, []byte(htmlified), 0o644)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
